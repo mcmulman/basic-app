7bdc173496b016f999f40c8388f85aaa1ace76a6
mcmulman <31311541+mcmulman@users.noreply.github.com>
2020-02-09 13:28:32 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="dist/main.js" defer></script>
+    <link rel="stylesheet" href="dist/main.css">
+    <title>My app</title>
+</head>
+<body>
+    <header>
+        ### HEADER ###
+    </header>
+    <main>
+        ### MAIN ###
+    </main>
+    <footer>
+        ### FOOTER ###
+    </footer>
+</body>
+</html>
