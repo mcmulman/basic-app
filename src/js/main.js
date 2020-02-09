b285a7848401bdeca10754d29943ec87ca9b4f68
mcmulman <github@klickdienst.de>
2020-02-09 17:40:06 +0100

    Add mobile navigation

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,14 @@ const test = () => {
 };
 
 test();
+
+const $navTrigger = document.getElementById('toggle'),
+      $nav = document.querySelector('.navbar ul');
+
+if ($navTrigger) {
+    $navTrigger.addEventListener('click', (e) => {
+        $nav.classList.toggle('visible');
+        e.preventDefault();
+        e.stopPropagation();
+    });
+}
